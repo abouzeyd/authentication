602aef83fca76a3c5e09110aa351d9435233feb2
mohou <mohoumouhamed01@gmail.com>
2021-08-25 12:30:05 +0000

    feat:moveComponent

---
+++ pages/component/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import fire from '../pages/config/fire';
+
+const Login = () => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    //   fire.auth().
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <p>hello</p>
+      <input
+        type="text"
+        placeholder="email"
+        name="email"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        name="password"
+        onChange={handleChange}
+      />
+      <button onClick={handleSubmit}>submit</button>
+    </div>
+  );
+};
+
+export default Login;
